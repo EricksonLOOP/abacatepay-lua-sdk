e07e18c72710379f1dc7c7d218b62e0b455151ee
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2024-12-20 10:56:16 -0300

    create/AbacatePayClientRequest
    Teste primeiro Objeto

---
+++ src/Models/AbacatePayClient/AbacatePayClient.lua
@@ -0,0 +1,11 @@
+AbacatePayClientRequest = {};
+AbacatePayClientRequest._index = AbacatePayClientRequest;
+function AbacatePayClientRequest:new(name, cellphone, email, taxId)
+    local AbacatePayClientObj = {}
+    AbacatePayClientObj.name = name;
+    AbacatePayClientObj.cellphone = cellphone;
+    AbacatePayClientObj.email = email;
+    AbacatePayClientObj.taxId = taxId;
+    setmetatable(AbacatePayClientObj, AbacatePayClientRequest);
+    return AbacatePayClientObj;
+end
